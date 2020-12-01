408ba374e9d140256e15a1cd5d2d31fbe11a65ba
Filip Seman <seman.filip@gmail.com>
2020-12-01 17:20:57 +0100

    cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,12 @@ set -x LC_COLLATE "C"
 set -x EDITOR nvim
 set -x VEDITOR code
 
-# fix IntelliJ apps blurr bug
+# IntelliJ fix, UI blurr/freez
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 
-set -Ua fish_user_paths ~/.local/bin /usr/local/bin /usr/sbin /usr/sbin /bin
+# Paths
+set -x fish_user_paths ~/.local/bin /usr/local/bin /usr/sbin /usr/sbin /bin
 
 alias xclip="xclip -selection clipboard"
 alias ll='ls -Flh --group-directories-first --color'
 alias l='ls -Fhla --group-directories-first --color'
-
