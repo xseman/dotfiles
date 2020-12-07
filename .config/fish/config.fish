0b9bdffe9c25f2a8d5780be9eaff36d548aebcdb
Filip Seman <seman.filip@gmail.com>
2020-12-07 10:13:18 +0100

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,9 @@ set -x VEDITOR code
 # IntelliJ fix, UI blurr/freez
 set -x _JAVA_AWT_WM_NONREPARENTING 1
 
+# Keybinds
+bind \cH backward-kill-word
+
 alias xclip="xclip -selection clipboard"
 alias ll='ls -Flh --group-directories-first --color'
 alias l='ls -Fhla --group-directories-first --color'
