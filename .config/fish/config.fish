1d50f8e3c20c86a14451d6a15b0f509cfb8080c3
Filip Seman <filip.seman@protonmail.com>
2022-02-23 23:31:33 +0100

    Cleanup shell envinroment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,17 @@ set fish_greeting
 # Keybinds
 bind \cH backward-kill-word
 
+# LS_COLORS
+eval (dircolors -c ~/.dircolors)
+
+# Aliases
+alias df="df -h"
+alias egrep="egrep -i --colour=auto"
+alias fgrep="fgrep -i --colour=auto"
+alias free="free -h"
+alias temp="cd (mktemp -d)"
 alias xclip="xclip -selection clipboard"
-alias ll='ls -Flh --group-directories-first --color'
-alias l='ls -Fhla --group-directories-first --color'
+alias grep="grep -sI --color=auto"
+alias ll="ls -Flh --group-directories-first --color"
+alias l="ls -Fhla --group-directories-first --color"
+alias ls="ls --group-directories-first --quoting-style=literal --show-control-chars --indicator-style=slash --color"
