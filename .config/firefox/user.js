7d8bbf60207b1d71ac70da8014272557e376ce6b
Filip Seman <filip.seman@protonmail.com>
2022-08-11 09:44:31 +0200

    firefox: update conf

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -2,6 +2,3 @@ user_pref("ui.key.menuAccessKeyFocuses", "false");
 user_pref("browser.tabs.loadBookmarksInBackground", "true");
 user_pref("findbar.modalHighlight", "true");
 user_pref("browser.tabs.closeWindowWithLastTab", "false");
-
-// Fix lag if there are at least 2 firefox instances on different monitors
-user_pref("gfx.webrender.force-legacy-layers", "false");
