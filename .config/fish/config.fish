47ba853e3537bf505dc659b61148cb6977f0341f
Filip Seman <filip.seman@protonmail.com>
2022-11-10 18:18:42 +0100

    chore(fish): sort a fix wrong alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,21 +8,21 @@ bind \cH backward-kill-word
 eval (dircolors -c ~/.dircolors)
 
 # Aliases
+alias cal="ncal -M -b"
+alias ctop="ctop -i"
 alias df="df -h"
 alias egrep="egrep -i --colour=auto"
+alias fd="fd --hidden"
 alias fgrep="fgrep -i --colour=auto"
 alias free="free -h"
 alias grep="grep -sI --color=auto"
+alias ip="ip -c"
 alias l="ls -Fhla --group-directories-first --color"
 alias ll="ls -Flh --group-directories-first --color"
 alias ls="ls --group-directories-first --quoting-style=literal --show-control-chars --indicator-style=slash --color"
 alias temp="cd (mktemp -d)"
 alias tm="tmux attach || tmux"
 alias xclip="xclip -selection clipboard"
-alias ip="ip -c"
-alias ctop="ctop -i"
-alias ctop="fd --hidden"
-alias cal="ncal -M -b || cal"
 
 if type -q "code-oss"
 	alias code="code-oss || code"
