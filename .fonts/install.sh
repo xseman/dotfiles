522a35ced660c1e871ee8f0edb89c0d6544a8e60
Filip Seman <filip.seman@protonmail.com>
2022-11-28 01:17:49 +0100

    config(font): install script

--- .fonts/install.sh
+++ .fonts/install.sh
@@ -3,9 +3,7 @@
 
 cd "$(dirname "${0}")" || exit 1
 
-mkdir -m 0777 -p ~/.fonts && sudo cp -rv ./* ~/.fonts/
-
+mkdir -p ~/.fonts && cp -rv ./* ~/.fonts/
 fc-cache -fv
-# fc-list
 
 echo "Run fc-list to view installed fonts..."
