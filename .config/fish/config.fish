38dc0bd7a2b9ad50b244df1deaff695cd7e3f8af
Filip Seman <filip.seman@protonmail.com>
2023-01-18 22:57:59 +0100

    chore: shell less colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,20 @@
 # Remove default greeting
 set fish_greeting
 
+# envs
+set -x LESS_TERMCAP_mb (printf "\e[34m")
+set -x LESS_TERMCAP_md (printf "\e[1;30m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[30;43m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[32m")
+
 # Paths v3.2.0
-fish_add_path ~/.deno/bin \
 fish_add_path ~/.local/bin \
+fish_add_path ~/.deno/bin \
 fish_add_path ~/go/bin \
+
 fish_add_path /usr/local/go/bin \
 fish_add_path /usr/local/bin \
 fish_add_path /usr/sbin \
