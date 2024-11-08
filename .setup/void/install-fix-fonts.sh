529719b269161ea11b9db57be8925e8b17da0f60
Filip Seman <filip.seman@pm.me>
2024-11-08 23:26:19 +0100

    setup: delete void

--- .setup/void/install-fix-fonts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://docs.voidlinux.org/config/graphical-session/fonts.html
-
-sudo ln -sf /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
-sudo xbps-reconfigure -f fontconfig
-
-echo "Font fix done..."
